1e3f9efa426ce05cd3dbb1e2e1b55d009aadb750
Divy Anant Varshney <divyanantx@gmail.com>
2025-08-12 17:21:50 +0530

    add category of products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from '../../components/nav/Nav'
 import bg from '../../assets/bg0.gif'
 import './home.css'
+import { category } from '../../category'
 
 function home() {
   return (
@@ -9,6 +10,17 @@ function home() {
       <div className="hero-bg">
         <img src={bg} alt="" />
       </div>
+
+      <div className="category-section">
+         {category.slice(0,5).map((item)=>(
+           <div className="category-card">
+            <img src={item.image} alt="" />
+            <span>{item.name}</span>
+           </div>
+        ))}
+      
+      </div>
+       
     </div>
   )
 }
